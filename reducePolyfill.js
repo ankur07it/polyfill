0908259ffa155e5c3117e64afc9f3f398dae3cec
Ankur Jain <ankurankur123@gmail.com>
2021-09-14 23:43:31 +0530

    Create reducePolyfill.js

---
+++ reducePolyfill.js
@@ -0,0 +1,13 @@
+
+Array.prototype.ReduceCall = function (callback, initialValue) {
+  var accumulator = initialValue === undefined ? undefined : initialValue;
+
+  for (var i = 0; i < this.length; i++) {
+    if (accumulator !== undefined) {
+      accumulator = callback.call(undefined, accumulator, this[i], i, this)
+    } else {
+      accumulator = this[i];
+    }
+  }
+  return accumulator;
+}
