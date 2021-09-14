319827ed1fbe0a101e4352d9d965a72cbf412317
Ankur Jain <ankurankur123@gmail.com>
2021-09-14 23:45:07 +0530

    Update filterPolyfill.js

--- filterPolyfill.js
+++ filterPolyfill.js
@@ -1,8 +1,8 @@
 
-Array.prototype.FilterCall = function (callback, context) {
+Array.prototype.FilterCall = function (callback) {
   let newArr = [];
   for (let i = 0; i < this.length; i++) {
-    if (callback.call(context, this[i], i, this))
+    if (callback.call(this, this[i], i, this))
       newArr.push(this[i]);
   }
   return newArr;
